46ea670a6db5578d8c66ab4d7ef01d96a2641a12
winnie688801 <winnie688801@gmail.com>
2018-10-16 16:32:21 +0800

    translate into Chinese

--- app/src/main/java/com/tom/bmi/MainActivity.java
+++ app/src/main/java/com/tom/bmi/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText edWeight;
     private EditText edHeight;
+    private Button help;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,22 +22,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         edWeight = findViewById(R.id.ed_weight);
         edHeight = findViewById(R.id.ed_height);
+        help = findViewById(R.id.help);
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("MainActivity", "onClick: help");
+                new AlertDialog.Builder(MainActivity.this)
+                .setMessage(R.string.bmi_info)
+                        .setPositiveButton(R.string.ok, null)
+                        .show();
+                            }
+        });
     }
 
     public void bmi(View view) {
         Log.d("MainActivity" , "bmi");
         String w = edWeight.getText().toString();
         String h = edHeight.getText().toString();
-        Log.d("MainActivity" , "bmi");
+        Log.d("MainActivity" , w +"/" + h);
         float weight = Float.parseFloat(w);
         float height = Float.parseFloat(h);
         float bmi = weight /(height*height);
         Log.d("MainActivity" , bmi+"");
-        Toast.makeText(this,"Your BMI is " +bmi , Toast.LENGTH_LONG).show();
+        Toast.makeText(this,getString(R.string.your_bmi_is) +bmi , Toast.LENGTH_LONG).show();
         new AlertDialog.Builder(this)
-                .setMessage("Your BMI is " + bmi)
-                .setTitle("BMI")
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                .setMessage(getString(R.string.your_bmi_is) + bmi)
+                .setTitle(R.string.bmi)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         edWeight.setText("");
